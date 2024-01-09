427b5726c602e5ed1846e5ccb8aee4e05b8b1652
Exploshe <stevenluu10@gmail.com>
2024-01-09 04:19:34 -0800

    Fleshed out characters page, implemented portray count and filter by afflatus/rarity

---
+++ data/default_character_count.js
@@ -0,0 +1,292 @@
+export const default_character_count = {
+    "Ms. Radio": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Door": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "The Fool": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "La Source": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "aliEn T": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Leilani": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "John Titor": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Twins Sleep": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Bette": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Darley Clatter": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "ONiON": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Sputnik": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Nick Bottom": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Eagle": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Зима": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Bunny Bunny": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Pavia": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Oliver Fog": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Mondlicht": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "APPLe": {
+        default: 1,
+        wish: 0,
+        manual: 0
+    },
+    "Cristallo": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "TTT": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Rabies": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Ms. Moissan": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Poltergeist": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Mesmer Jr.": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Erick": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "X": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Sweetheart": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Baby Blue": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Charlie": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Bkornblume": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Dikke": {
+        default: 1,
+        wish: 0,
+        manual: 0
+    },
+    "Sonetto": {
+        default: 4,
+        wish: 0,
+        manual: 0
+    },
+    "Balloon Party": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Necrologist": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Satsuki": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Matilda Bouanich": {
+        default: 1,
+        wish: 0,
+        manual: 0
+    },
+    "Tennant": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Click": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Diggers": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Blonney": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Horropedia": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Druvis III": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Lilya": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "A Knight": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Sotheby": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Regulus": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Centurion": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "An-an Lee": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Medicine Pocket": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Voyager": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Eternity": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Ms. NewBabel": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Melania": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Pickles": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Tooth Fairy": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+    "Jessica": {
+        default: 0,
+        wish: 0,
+        manual: 0
+    },
+}
---
+++ scripts/characters.js
@@ -0,0 +1,78 @@
+import { characterIds } from "../data/character_ids.js";
+import { characterNames } from "../data/character_ids.js";
+import { default_character_count } from "../data/default_character_count.js";
+
+const filterbuttons = document.querySelectorAll(".sort-filter-bar div");
+const characterList = document.querySelectorAll(".characters a");
+
+filterbuttons.forEach(button => {{
+    button.addEventListener("click", () => {
+        button.classList.contains("selected") ? button.classList.remove("selected") : button.classList.add("selected")
+        updateFilters();
+    });
+}})
+
+function updateFilters() {
+    const afflatusFilters = getSelectedFilters("afflatus");
+    const rarityFilters = getSelectedFilters("rarity");
+
+    const filteredCharacters = []
+    characterList.forEach(characterElement => {
+        // characterElement.children[3] -> <div class="class0 class1 _ID"></div>, .classList[2] -> _ID, .slice(1) -> ID
+        const id = characterElement.children[3].classList[2].slice(1);
+        const character = characterIds[id];
+        if ((!rarityFilters.length || rarityFilters.includes(character.rarity.toString())) && (!afflatusFilters.length || afflatusFilters.includes(character.afflatus))) {
+            filteredCharacters.push(characterElement);
+        }
+    });
+
+    displayFilteredCharacters(filteredCharacters);
+}
+
+function getSelectedFilters(filterType) {
+    return Array.from(filterbuttons)
+        .filter(button => button.classList.contains("selected") && button.classList.contains(filterType))
+        .map(button => button.classList[0]);
+}
+
+function displayFilteredCharacters(filteredCharacters) {
+    characterList.forEach(characterElement => {
+        characterElement.style.display = "none";
+    });
+
+    filteredCharacters.forEach(characterElement => {
+        characterElement.style.display = "flex";
+    });
+}
+
+
+// Count characters and set portray amount
+const summonData = JSON.parse(localStorage.getItem("summonData"));
+// const characters = localStorage.getItem("characters") ? JSON.parse(localStorage.getItem("characters")) : default_character_count;
+const characters = default_character_count;
+
+if (!localStorage.getItem("characters")) {
+    for (const [bannerType, obj] of Object.entries(summonData)) {
+        for (let i = 0; i < obj.history.length; i++) {
+            const summon = obj.history[i];
+            if (summon.name === "3има") {
+                characters["Зима"].wish += 1
+                continue
+            }
+            if (summon.name.startsWith("The Golden Thread")) {
+                continue
+            }
+            characters[summon.name].wish += 1
+        }
+    }
+
+    // localStorage.setItem("characters", JSON.stringify(characters));
+}
+
+for (const [name, obj] of Object.entries(characters)) {
+    const portray = document.querySelector(`._${characterNames[name]}`);
+    const count = obj.default + obj.wish + obj.manual;
+    portray.innerHTML = count ? `P${count - 1}` : "";
+}
+
+
