0aaa65f89060792099df6a6f964807bded2ec7c2
Exploshe <stevenluu10@gmail.com>
2024-01-23 18:42:44 -0800

    Added start and end datetimes to banners

--- data/banners.js
+++ data/banners.js
@@ -1,107 +1,175 @@
 export const banners = {
-    "望族与隐士": {
-        rateUp6StarId: 3009,
-        rateUp5StarIds: [],
-        img: ""
+    // beginner banner
+    "第一滴雨": {
+        rateUp6StarId: null,
+        rateUp5StarIds: null,
+        img: null,
+        start: "2023-10-26 00:00:00",
+        end: null
     },
+    // standard banner
+    "于湖中央": {
+        rateUp6StarId: null,
+        rateUp5StarIds: null,
+        img: null,
+        start: "2023-10-26 00:00:00",
+        end: null
+    },
+    // invitation from the water
+    "湖的邀约": {
+        rateUp6StarId: null,
+        rateUp5StarIds: null,
+        img: null,
+        start: "2023-12-30 00:00:00",
+        end: "2024-01-13 04:59:59"
+    },
+    // reruns
     "剑与盔的嘶鸣": {
         rateUp6StarId: 3007,
         rateUp5StarIds: [3043, 3020],
-        img: "images/banners/Clang_of_Sword_and_Armor_Banner.webp"
-    },
-    "流行即世界": {
-        rateUp6StarId: 3062,
-        rateUp5StarIds: [3011, 3024],
-        img: "images/banners/Pop_is_Everything_Banner.webp"
-    },
-    "星的栖居": {
-        rateUp6StarId: 3048,
-        rateUp5StarIds: [3010, 3024],
-        img: "images/banners/Where_the_Star_Alighted_Banner.webp"
-    },
-    "牧羊犬如是说": {
-        rateUp6StarId: 3063,
-        rateUp5StarIds: [3064, 3016],
-        img: "images/banners/Thus_Spoke_the_Border_Collie_Banner.webp"
-    },
-    "深林的絮语": {
-        rateUp6StarId: 3003,
-        rateUp5StarIds: [3049, 3037],
-        img: "images/banners/Whisper_of_the_Woods_Banner.webp"
-    },
-    "弩箭破空而至": {
-        rateUp6StarId: 3004,
-        rateUp5StarIds: [],
-        img: ""
-    },
-    "仙子振翅入夜": {
-        rateUp6StarId: 3053,
-        rateUp5StarIds: [3060, 3020],
-        img: "images/banners/The_Fairies_Shining_at_Night_Banner.webp"
-    },
-    "自由摇摆": {
-        rateUp6StarId: 3025,
-        rateUp5StarIds: [3022, 3038],
-        img: "images/banners/Swinging_Freely_Banner.webp"
-    },
-    "精灵仍在等待": {
-        rateUp6StarId: 3056,
-        rateUp5StarIds: [3061, 3037],
-        img: "images/banners/The_Changeling_Awaits_Banner.webp"
-    },
-    "午夜放映会": {
-        rateUp6StarId: 3039,
-        rateUp5StarIds: [3016, 3011],
-        img: "images/banners/Midnight_Movie_Party_Banner.webp"
-    },
-    "涌流无止之物": {
-        rateUp6StarId: 3051,
-        rateUp5StarIds: [3017, 3049],
-        img: "images/banners/The_Ever-Flowing_Banner.webp"
-    },
-    "回到融雪处": {
-        rateUp6StarId: 3072,
-        rateUp5StarIds: [],
-        img: ""
-    },
-    "众光之光": {
-        rateUp6StarId: 3032,
-        rateUp5StarIds: [3010, 3022],
-        img: "images/banners/Focus_of_the_Lights_Banner.webp"
-    },
-    "晚星所纺织的": {
-        rateUp6StarId: 3070,
-        rateUp5StarIds: [3071, 3043],
-        img: "images/banners/v1a3_galapona_1.png"
-    },
-    "金标破坏王": {
-        rateUp6StarId: 3047,
-        rateUp5StarIds: [],
-        img: ""
-    },
-    "大讲演时代": {
-        rateUp6StarId: 3052,
-        rateUp5StarIds: [],
-        img: ""
-    },
-    "现象未漫及处": {
-        rateUp6StarId: 3066,
-        rateUp5StarIds: [],
-        img: ""
-    },
-    "真理的壁影": {
-        rateUp6StarId: 3079,
-        rateUp5StarIds: [],
-        img: ""
+        img: "images/banners/Clang_of_Sword_and_Armor_Banner.webp",
+        start: "2023-10-26 00:00:00",
+        end: "2023-11-09 04:59:59"
     },
     "一克“好奇心”": {
         rateUp6StarId: 3009,
         rateUp5StarIds: [3010, 3017],
-        img: "images/banners/One_Gram_of_Curiosity_Banner.webp"
+        img: "images/banners/One_Gram_of_Curiosity_Banner.webp",
+        start: "2023-10-26 00:00:00",
+        end: "2023-11-09 04:59:59"
+    },
+    "深林的絮语": {
+        rateUp6StarId: 3003,
+        rateUp5StarIds: [3049, 3037],
+        img: "images/banners/Whisper_of_the_Woods_Banner.webp",
+        start: "2023-11-09 05:00:00",
+        end: "2023-11-21 04:59:59"
+    },
+    "自由摇摆": {
+        rateUp6StarId: 3025,
+        rateUp5StarIds: [3022, 3038],
+        img: "images/banners/Swinging_Freely_Banner.webp",
+        start: "2023-11-21 05:00:00",
+        end: "2023-12-05 04:59:59"
+    },
+    "星的栖居": {
+        rateUp6StarId: 3048,
+        rateUp5StarIds: [3010, 3024],
+        img: "images/banners/Where_the_Star_Alighted_Banner.webp",
+        start: "2023-12-05 05:00:00",
+        end: "2023-12-19 04:59:59"
+    },
+    "涌流无止之物": {
+        rateUp6StarId: 3051,
+        rateUp5StarIds: [3017, 3049],
+        img: "images/banners/The_Ever-Flowing_Banner.webp",
+        start: "2023-12-19 05:00:00",
+        end: "2024-01-02 04:59:59"
+    },
+    "午夜放映会": {
+        rateUp6StarId: 3039,
+        rateUp5StarIds: [3016, 3011],
+        img: "images/banners/Midnight_Movie_Party_Banner.webp",
+        start: "2024-01-02 05:00:00",
+        end: "2024-01-16 04:59:59"
+    },
+    "众光之光": {
+        rateUp6StarId: 3032,
+        rateUp5StarIds: [3010, 3022],
+        img: "images/banners/Focus_of_the_Lights_Banner.webp",
+        start: "2024-01-16 05:00:00",
+        end: "2024-01-30 04:59:59"
+    }, 
+    // new arcanist banners
+    "流行即世界": {
+        rateUp6StarId: 3062,
+        rateUp5StarIds: [3011, 3024],
+        img: "images/banners/Pop_is_Everything_Banner.webp",
+        start: "2023-11-09 05:00:00",
+        end: "2023-11-23 04:59:59"
+    },
+    "牧羊犬如是说": {
+        rateUp6StarId: 3063,
+        rateUp5StarIds: [3064, 3016],
+        img: "images/banners/Thus_Spoke_the_Border_Collie_Banner.webp",
+        start: "2023-11-23 05:00:00",
+        end: "2023-12-07 04:59:59"
+    },
+    "仙子振翅入夜": {
+        rateUp6StarId: 3053,
+        rateUp5StarIds: [3060, 3020],
+        img: "images/banners/The_Fairies_Shining_at_Night_Banner.webp",
+        start: "2023-12-07 05:00:00",
+        end: "2023-12-28 04:59:59"
+    },
+    "精灵仍在等待": {
+        rateUp6StarId: 3056,
+        rateUp5StarIds: [3061, 3037],
+        img: "images/banners/The_Changeling_Awaits_Banner.webp",
+        start: "2023-12-28 05:00:00",
+        end: "2024-01-18 04:59:59"
+    },
+    "晚星所纺织的": {
+        rateUp6StarId: 3070,
+        rateUp5StarIds: [3071, 3043],
+        img: "images/banners/v1a3_galapona_1.png",
+        start: "2024-01-18 05:00:00",
+        end: "2024-02-08 04:59:59"
+    },
+    "回到融雪处": {
+        rateUp6StarId: 3072,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
+    },
+    "弩箭破空而至": {
+        rateUp6StarId: 3004,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
+    },
+    "望族与隐士": {
+        rateUp6StarId: 3009,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
+    },
+    "金标破坏王": {
+        rateUp6StarId: 3047,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
+    },
+    "大讲演时代": {
+        rateUp6StarId: 3052,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
+    },
+    "现象未漫及处": {
+        rateUp6StarId: 3066,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
+    },
+    "真理的壁影": {
+        rateUp6StarId: 3079,
+        rateUp5StarIds: [],
+        img: "",
+        start: "",
+        end: ""
     },
     "火花雀儿": {
         rateUp6StarId: 3073,
         rateUp5StarIds: [],
-        img: ""
+        img: "",
+        start: "",
+        end: ""
     },
 }
