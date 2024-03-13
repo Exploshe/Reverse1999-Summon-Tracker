1e07550759dca141938c1220a48eb5078c65d2f3
Exploshe <stevenluu10@gmail.com>
2024-03-12 20:10:02 -0700

    Added back import and added pickles rerun banner

--- data/banners.js
+++ data/banners.js
@@ -124,13 +124,22 @@ export const banners = {
         start: "2023-11-09 05:00:00",
         end: "2023-11-23 04:59:59"
     },
-    "牧羊犬如是说": {
+    "牧羊犬如是说": [  // Pickles
+    {
         rateUp6StarId: 3063,
         rateUp5StarIds: [3064, 3016],
         img: "images/banners/Thus_Spoke_the_Border_Collie_Banner.webp",
         start: "2023-11-23 05:00:00",
         end: "2023-12-07 04:59:59"
     },
+    {  // rerun
+        rateUp6StarId: 3063,  
+        rateUp5StarIds: [3024, 3037],
+        img: "images/banners/Thus_Spoke_the_Border_Collie_Banner.webp",
+        start: "2024-03-12 05:00:00",
+        end: "2024-03-26 04:59:59"
+    },
+    ],
     "仙子振翅入夜": {
         rateUp6StarId: 3053,
         rateUp5StarIds: [3060, 3020],
