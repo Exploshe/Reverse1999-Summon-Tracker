35ca72a3de1dba742e7b795fdb829346a365e44f
Exploshe <stevenluu10@gmail.com>
2025-06-10 11:42:00 -0700

    added Tuesday rerun

--- data/banners.js
+++ data/banners.js
@@ -564,13 +564,22 @@ export const banners = {
         start: "2024-11-21 05:00:00",
         end: "2024-12-05 04:59:59"
     },
-    "午夜摇篮曲": {
-        rateUp6StarId: 3098,  // Tuesday
-        rateUp5StarIds: [3099, null],
-        img: "images/banners/Blue_Lullaby_Banner.png",
-        start: "2024-12-05 05:00:00",
-        end: "2024-12-26 04:59:59"
-    },
+    "午夜摇篮曲": [  // Tuesday
+        {
+            rateUp6StarId: 3098,
+            rateUp5StarIds: [3099, null],
+            img: "images/banners/Blue_Lullaby_Banner.png",
+            start: "2024-12-05 05:00:00",
+            end: "2024-12-26 04:59:59"
+        },
+        {
+            rateUp6StarId: 3098,
+            rateUp5StarIds: [3078, 3082],
+            img: "images/banners/Blue_Lullaby_Banner.png",
+            start: "2025-06-03 05:00:00",
+            end: "2025-06-17 04:59:59"
+        },
+    ],
     "目标即盲点": {
         rateUp6StarId: 3097,  // Argus
         rateUp5StarIds: [3043, 3038],
